0021e37dcd4a0a5f0ea2e753addd99e6ccf6376a
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-07-26 22:14:34 +0800

    Record mouse move only and can play again and again with clear, start, stop and play functionality

---
+++ MouseRec/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace MouseRec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(101, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 146);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(252, 115);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "X";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Y";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(252, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 305);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MouseRec/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseRec
+{
+    public partial class Form1 : Form
+    {
+        ListViewItem lv;
+        int a, b;
+
+        public Form1()
+        {
+            InitializeComponent();
+        } 
+
+
+        /**
+        * Auto Playing the recorded coordinates
+        */
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(a!=b)
+            {  
+                Cursor.Position = new Point(
+                    int.Parse(listView1.Items[a].SubItems[0].Text), 
+                    int.Parse(listView1.Items[a].SubItems[1].Text)
+                );
+                a++; 
+            }
+            else
+            {
+                // if cursor playing is done then
+                // cursor should start back to zero so that when user play again 
+                // it should work and start playing from the start
+                //a = 0; 
+            }
+        }
+             
+       /**
+       * Recording mouse coordinates
+       */
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            lv = new ListViewItem(Cursor.Position.X.ToString());
+            lv.SubItems.Add(Cursor.Position.Y.ToString()); 
+            listView1.Items.Add(lv);
+            b++;
+        }
+
+        /** 
+        * Start recording
+        */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); 
+        }
+
+
+        /**
+        * Stop button
+        */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+
+            
+        }
+
+
+        /**
+        * Main form
+        */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+        }
+
+
+        /**
+        * Clear button
+        */
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            listView1.Items.Clear();
+
+            timer2.Stop();
+            timer1.Stop();
+
+            a = 0;
+            b = 0;
+        }
+
+
+        /**
+        * Start button
+        */
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+
+            a = 0;
+
+        }
+    }
+}
